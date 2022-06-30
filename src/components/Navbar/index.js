a742fb23a5c8bbe1e8f0b4c85839873d9c87c7ad
Adela <adelangr822@gmail.com>
2022-06-30 15:14:04 +0800

    update navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,9 +8,23 @@ const Navbar = () => {
         className="navbar navbar-expand-lg navbar-light bg-light px-5 py-4 "
         data-testid="navbar"
       >
-        <a className="navbar-brand" href={"/"}>
+        <a className="navbar-brand" href={"/home"}>
           CAR BOOKING
         </a>
+        <div className="collapse navbar-collapse ">
+          <ul className="navbar-nav">
+            <li className="nav-item ">
+              <a className="nav-link" href={"/home"}>
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href={"/"}>
+                Requsted
+              </a>
+            </li>
+          </ul>
+        </div>
         <div className="logout">
           <button data-testid="button-logout" className="btn btn-outline-dark">
             Logout
