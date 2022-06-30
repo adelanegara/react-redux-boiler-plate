ab9f09ff27d3a9a54b90c952ab9ca3f5d25af8a0
Adela <adelangr822@gmail.com>
2022-06-30 14:45:24 +0800

    fetch data carOption

--- src/components/UserHomePage/index.js
+++ src/components/UserHomePage/index.js
@@ -1,9 +1,9 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const UserHomePage = () => {
+const UserHomePage = ({ carsOption }) => {
   return (
     <div>
-      {" "}
       <div className="row m-0 ">
         <div className="col-3 ">
           <div className="card border-0 m-5">
@@ -24,41 +24,42 @@ const UserHomePage = () => {
         </div>
         <div className="col-9 ">
           <div className="row mt-5">
-            <div className="col-4 ">
-              <div className="ui link cards">
-                <div className="card shadow-sm p-3 mb-5 bg-white ">
-                  <div className="image">
-                    {/* <img src={item.image} alt={item.id} /> */}
-                  </div>
-                  <div className="rate text-center">
-                    <p> AVAILABLE </p>
-                  </div>
-                  <div className="content content-product">
-                    <div className="category-text">
-                      <span>category: sedan </span>
+            {carsOption.map((item, index) => {
+              return (
+                <>
+                  <div className="col-4  " key={index}>
+                    <div className="ui link cards">
+                      <div className="card shadow-sm p-3 mb-5 bg-white ">
+                        <div className="image">
+                          <img src={item.image} alt={item.id} />
+                        </div>
+                        <div className="rate text-center">
+                          <p> {item.status} </p>
+                        </div>
+                        <div className="content content-product">
+                          <div className="category-text">
+                            <span>{item.type} </span>
+                          </div>
+                          <div className="header">Toyota Vios</div>
+                          <div className="category-text pb-2">
+                            <span>20 pieces available </span>
+                          </div>
+                          <div className="form-group d-flex justify-content-between">
+                            <button
+                              type="button"
+                              className="btn btn-sm btn-dark button-shop"
+                            >
+                              Book
+                            </button>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div className="header">Toyota Vios</div>
-                    <div className="category-text pb-2">
-                      <span>20 pieces available </span>
-                    </div>
-                    <div className="form-group d-flex justify-content-between">
-                      <button
-                        type="button"
-                        className="btn btn-sm btn-dark button-shop"
-                      >
-                        Book
-                      </button>
-                      {/* <Link
-                      to={`/view/${item.id}`}
-                      className="btn btn-sm btn-dark button-shop"
-                    >
-                      View
-                    </Link> */}
-                    </div>
                   </div>
-                </div>
-              </div>
-            </div>
+                  ;
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -66,4 +67,8 @@ const UserHomePage = () => {
   );
 };
 
-export default UserHomePage;
+const mapStateToProps = (state) => ({
+  carsOption: state.carsOption,
+});
+
+export default connect(mapStateToProps)(UserHomePage);
